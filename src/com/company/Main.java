6ef39e4fbeaf15ea365f654bd8fe596d858e509d
NRS <normas@wp.pl>
2018-08-12 10:29:42 +0200

    Dodanie metody do wyswietlania tekstu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        wyswietlTekst("TEST");
+    }
+
+    public static void wyswietlTekst(String tekst) {
+        System.out.println("Text: " + tekst);
     }
 }
